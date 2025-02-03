bc1073f76ae0924c862b2dc6b0ba1e7a0f06eb58
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-02-02 16:08:58 -0800

    using functions in demo

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
 import json
 
 from queries import *
+from queries import build_teacher_search_query
 from rmp_wrapper import RMPWrapper
 
 rmp = RMPWrapper()
 
-NEW_SEARCH_SCHOOLS_QUERY_VARIABLES = {
-    "query": {
-        "text": "", # School name here, more specific the better
-    }
-}
+SEARCH_SCHOOL_VARIABLES = build_school_search_query("School name")
 
-NEW_SEARCH_TEACHERS_QUERY_VARIABLES = {
-    "query": {
-        "text": "", # Prof name here
-        "schoolID": "" # Use what you got from the query above (id not legacyId)
-    },
-    "count": 10, # Found this was a good number for search result count
-}
+SEARCH_PROF_VARIABLES = build_teacher_search_query("Prof name (more specific the better)", "School ID, not legacyId")
 
-TEACHER_RATINGS_PAGE_QUERY_VARIABLES = {
-    "id": "" # Prof id here from query above
-}
+GET_RATINGS_VARIABLES = build_rating_query("Prof ID from previous query, not legacyId")
+
 
 if __name__ == "__main__":
-    response = rmp.execute_query(NEW_SEARCH_SCHOOLS_QUERY, NEW_SEARCH_SCHOOLS_QUERY_VARIABLES)
+    response = rmp.execute_query(NEW_SEARCH_SCHOOLS_QUERY, SEARCH_SCHOOL_VARIABLES)
     print(json.dumps(response, indent=4))
-    # response1 = rmp.execute_query(NEW_SEARCH_TEACHERS_QUERY, NEW_SEARCH_TEACHERS_QUERY_VARIABLES)
-    # print(json.dumps(response1, indent=4))
-    # response2 = rmp.execute_query(TEACHER_RATINGS_PAGE_QUERY, TEACHER_RATINGS_PAGE_QUERY_VARIABLES)
-    # print(json.dumps(response2, indent=4))
