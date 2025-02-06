5422320c0c9713601715c0073376869868fb1dd8
David Shubov <116600844+shooby-d@users.noreply.github.com>
2025-02-05 22:33:11 -0800

    tried to clean up main and leave examples, check README

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import json
 
 from queries import *
-from rmp_wrapper import RMPWrapper
+from rmp_wrapper import *
 
 rmp = RMPWrapper()
 
-SEARCH_SCHOOL_VARIABLES = build_school_search_query("School name")
+SEARCH_SCHOOL_VARIABLES = build_school_search_query("Your school")
 
-SEARCH_PROF_VARIABLES = build_teacher_search_query("Prof name (more specific the better)", "School ID, not legacyId")
+SEARCH_PROF_VARIABLES = build_teacher_search_query("Prof name", "School id")
 
-GET_RATINGS_VARIABLES = build_rating_query("Prof ID from previous query, not legacyId")
-
+SEARCH_RATINGS_VARIABLES = build_rating_query("Prof id")
 
 if __name__ == "__main__":
-    response = rmp.execute_query(NEW_SEARCH_SCHOOLS_QUERY, SEARCH_SCHOOL_VARIABLES)
-    print(json.dumps(response, indent=4))
+    # schools_response = rmp.execute_query(NEW_SEARCH_SCHOOLS_QUERY, SEARCH_SCHOOL_VARIABLES)
+    # print(json.dumps(schools_response, indent=4))
+
+    # profs_response = rmp.execute_query(NEW_SEARCH_TEACHERS_QUERY, SEARCH_PROF_VARIABLES)
+    # print(json.dumps(profs_response, indent=4))
+
+    # ratings_response = rmp.execute_query(RATINGS_SEARCH_QUERY, SEARCH_RATINGS_VARIABLES)
+    # print(json.dumps(ratings_response, indent=4))
+    pass
